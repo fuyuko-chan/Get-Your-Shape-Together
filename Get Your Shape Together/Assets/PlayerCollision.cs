dfb4eb88d4835a795e5c0d7950430b05040b3aee
EstebanPerez18 <s.perezesteban18@gmail.com>
2019-03-30 05:08:09 -0700

    trying to add audio

---
+++ Get Your Shape Together/Assets/PlayerCollision.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+    public GameObject deathEffect;
+    void OnCollisionEnter (Collision col)
+    {
+        if (col.collider.tag == "Meteor")
+        {
+            Instantiate(deathEffect, transform.position, transform.rotation);
+            GameManager.instance.EndGame();
+
+            FindObjectOfType<AudioManager>().Play("PlayerDeath");
+
+            Destroy(gameObject);
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
