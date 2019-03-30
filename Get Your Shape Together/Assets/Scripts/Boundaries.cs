9bddbcb37b7471e027a09db595ed4e1994720af4
Joaquin Alonzo <joaquinalonzo22@yahoo.com>
2019-03-30 09:08:14 -0700

    Other shapes to attach

--- Get Your Shape Together/Assets/Scripts/Boundaries.cs
+++ Get Your Shape Together/Assets/Scripts/Boundaries.cs
@@ -17,7 +17,25 @@ public class Boundaries : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // border X axis
+        if (transform.position.x <= -10.5f)
+        {
+            transform.position = new Vector2(-10.5f, transform.position.y);
+        }
+        else if (transform.position.x >= 10.5f)
+        {
+            transform.position = new Vector2(10.5f, transform.position.y);
+        }
 
+        // border Y axis
+        if (transform.position.y <= -3.8f)
+        {
+            transform.position = new Vector2(transform.position.x, -3.8f);
+        }
+        else if (transform.position.y >= 3.8f)
+        {
+            transform.position = new Vector2(transform.position.x, 3.8f);
+        }
 
     }
 }
