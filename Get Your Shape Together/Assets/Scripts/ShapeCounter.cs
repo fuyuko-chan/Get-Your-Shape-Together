6b15545f0e5b788ba3990cbfae9228ae1158ee6a
melissajg <mjgaines0@gmail.com>
2019-03-30 13:42:37 -0700

    working on shape counter

---
+++ Get Your Shape Together/Assets/Scripts/ShapeCounter.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShapeCounter : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public List<GameObject> attachedShapes = new List<GameObject>();
+    int sizeOfList;
+
+    void Start()
+    {
+        sizeOfList = attachedShapes.Count;
+        if (sizeOfList == 3)
+        {
+            Application.Quit();
+        }
+    }      
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        
+    }
+}
