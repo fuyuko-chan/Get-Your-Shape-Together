fa1625273fe4e78c68e41915b359dd032b3c33df
EstebanPerez18 <s.perezesteban18@gmail.com>
2019-03-29 23:04:41 -0700

    tried to make the attach function work with prefabs

--- Get Your Shape Together/Assets/Attacher.cs
+++ Get Your Shape Together/Assets/Attacher.cs
@@ -34,10 +34,9 @@ public class Attacher : MonoBehaviour
     //OnTriggerEnter2D is called whenever this object overlaps with a trigger collider.
     void OnTriggerEnter2D(Collider2D other)
     {
-        //Check the provided Collider2D parameter other to see if it is tagged "PickUp", if it is...
-        if (other.gameObject.CompareTag("PickUp"))
+        if (other.gameObject.CompareTag ("PickUp"))
         {
-            other.gameObject.SetActive(false);
+            other.gameObject.SetActive (false);
         }
     }
 }
