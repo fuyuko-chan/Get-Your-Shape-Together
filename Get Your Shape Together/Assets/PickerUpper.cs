4d48be734453bfeca898b25b31c5b27070b24049
EstebanPerez18 <s.perezesteban18@gmail.com>
2019-03-30 01:17:15 -0700

    renamed the triangle to player

---
+++ Get Your Shape Together/Assets/PickerUpper.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickerUpper : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Pickup();
+        }
+    }
+
+    void Pickup()
+    {
+        Debug.Log("Picked up a pick up!");
+    }
+}
