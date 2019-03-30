214263cca74a7bbb7d05aaaddfab1aca7c3349a4
melissajg <mjgaines0@gmail.com>
2019-03-29 22:45:33 -0700

    FIXED MOVEMENTS

--- Get Your Shape Together/Assets/Scripts/Movement.cs
+++ Get Your Shape Together/Assets/Scripts/Movement.cs
@@ -3,30 +3,43 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Movement : MonoBehaviour
-{ 
-public float speed;             //Floating point variable to store the player's movement speed.
-
-private Rigidbody2D rb2d;       //Store a reference to the Rigidbody2D component required to use 2D Physics.
+{
+    public float speed = 0.1f;      
 
 void Start()
 {
-    //Get and store a reference to the Rigidbody2D component so that we can access it.
-    rb2d = GetComponent<Rigidbody2D> ();
+   
 }
 
-//FixedUpdate is called at a fixed interval and is independent of frame rate. Put physics code here.
-void FixedUpdate()
+void Update()
 {
-    //Store the current horizontal input in the float moveHorizontal.
-    float moveHorizontal = Input.GetAxis ("Horizontal");
+    if (Input.GetKey(KeyCode.RightArrow))
+        {
+            Vector2 position = transform.position;
+            position.x += speed;
+            transform.position = position;
+        }
+    if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            Vector2 position = transform.position;
+            position.x -= speed;
+            transform.position = position;
+        }
+    if (Input.GetKey(KeyCode.UpArrow))
+        {
+            Vector2 position = transform.position;
+            position.y += speed;
+            transform.position = position;
+        }
+    if (Input.GetKey(KeyCode.DownArrow))
+        {
+            Vector2 position = transform.position;
+            position.y -= speed;
+            transform.position = position;
+        }
 
-    //Store the current vertical input in the float moveVertical.
-    float moveVertical = Input.GetAxis ("Vertical");
-
-    //Use the two store floats to create a new Vector2 variable movement.
-    Vector2 movement = new Vector2 (moveHorizontal, moveVertical);
-
-    //Call the AddForce function of our Rigidbody2D rb2d supplying movement multiplied by speed to move our player.
-    rb2d.AddForce (movement * speed);
 }
+
+
+
 }
