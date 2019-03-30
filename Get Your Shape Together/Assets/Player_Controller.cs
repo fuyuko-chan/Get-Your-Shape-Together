b58c4365e33e1e9638d26e1e051eca4fa8c5b9c9
EstebanPerez18 <s.perezesteban18@gmail.com>
2019-03-29 21:20:30 -0700

    Added more to my script....

--- Get Your Shape Together/Assets/Player_Controller.cs
+++ Get Your Shape Together/Assets/Player_Controller.cs
@@ -4,15 +4,8 @@ using UnityEngine;
 
 public class Player_Controller : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float speed; //floating point variable to store the player's movement speed.
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    private Rigidbody2D rb2d; //store a reference to the RigidBody2D component required to use 2D Physics.
+
 }
