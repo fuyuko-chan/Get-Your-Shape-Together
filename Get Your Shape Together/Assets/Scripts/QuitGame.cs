a4c0c702de6496f78818e58a358104be748079ea
Joaquin Alonzo <joaquinalonzo22@yahoo.com>
2019-03-30 13:29:43 -0700

    Quit Button

---
+++ Get Your Shape Together/Assets/Scripts/QuitGame.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitGame : MonoBehaviour
+{
+    public void QuitButtoon()
+    {
+        Application.Quit();
+        Debug.Log("Game is exiting");
+        //Just to make sure its working
+    }
+}
