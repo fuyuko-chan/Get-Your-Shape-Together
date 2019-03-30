ccd970d5da920da24095529ca9ccdf7803378cb1
melissajg <mjgaines0@gmail.com>
2019-03-30 02:52:42 -0700

    allows for collsions for colliders

--- Get Your Shape Together/Assets/Scripts/Movement.cs
+++ Get Your Shape Together/Assets/Scripts/Movement.cs
@@ -4,42 +4,46 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    public float speed = 0.1f;      
+    public float speed = 0.1f;
+    public float moveSpeed;
 
 void Start()
 {
    
 }
 
-void Update()
-{
-    if (Input.GetKey(KeyCode.RightArrow))
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.RightArrow))
         {
             Vector2 position = transform.position;
             position.x += speed;
             transform.position = position;
         }
-    if (Input.GetKey(KeyCode.LeftArrow))
+        if (Input.GetKey(KeyCode.LeftArrow))
         {
             Vector2 position = transform.position;
             position.x -= speed;
             transform.position = position;
         }
-    if (Input.GetKey(KeyCode.UpArrow))
+        if (Input.GetKey(KeyCode.UpArrow))
         {
             Vector2 position = transform.position;
             position.y += speed;
             transform.position = position;
         }
-    if (Input.GetKey(KeyCode.DownArrow))
+        if (Input.GetKey(KeyCode.DownArrow))
         {
             Vector2 position = transform.position;
             position.y -= speed;
             transform.position = position;
-        }
 
-}
+        // allows collsion with objects with collider
+       Vector3 moveDirection = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")); 
 
+     
+
+    }
 
 
 }
